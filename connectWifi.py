396613c2d418743c43ae463ed9eb89104f3f56eb
Ye Zhang <mr.yezhang@gmail.com>
2020-10-10 23:30:25 -0700

    Add connect wifi module

---
+++ connectWifi.py
@@ -0,0 +1,49 @@
+"""
+Side effect: turns off AP
+Connect to the ssid and pwd
+"""
+
+import time
+
+from micropython import const
+
+_STAT_GOT_IP = const(5)
+_wait_connection_milliseconds = const(10000)  # Do not wait too long, other WDT will reset MCU.
+
+
+def do_connect(ssid, pwd) -> bool:
+    """
+    Connect to wifi
+    :param ssid: ssid of the wifi
+    :param pwd: password
+    :return: True if connected successfully.
+    """
+    import network
+
+    print('Turn off AP for security.')
+    sta_ap = network.WLAN(network.AP_IF)
+    sta_ap.active(False)
+
+    sta_if = network.WLAN(network.STA_IF)
+
+    sta_if.active(False)
+    while sta_if.active():
+        pass
+
+    sta_if.active(True)
+    while not sta_if.active():
+        pass
+
+    print('Connecting to network', ssid, "...")
+
+    start = time.ticks_ms()
+    sta_if.connect(ssid, pwd)
+    while not sta_if.isconnected():
+        if time.ticks_diff(time.ticks_ms(), start) > _wait_connection_milliseconds:
+            return False
+
+    while sta_if.status() != _STAT_GOT_IP:
+        pass
+
+    print('Connected to network ', ssid, '. ifconfig is ', sta_if.ifconfig())
+    return True
